6df3bf2e7b16ca7115a39ab68b4645656dbbc864
sunshinexyj <sunshinexyj@qq.com>
2014-07-27 15:46:42 +0800

    add markdown.js module

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var markdown = require('markdown-js');
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res) {
-  res.send('respond with a resource');
+//  res.send('respond with a resource');
+    var html = markdown.makeHtml("[Java Eye](http://www.iteye.com/ \"Click\") ");
+    res.send(html)
+    res.end();
 });
 
 module.exports = router;
